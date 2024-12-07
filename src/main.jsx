6149c474531546eba392d5fa738af53f1113a681
aakritrajput <aakritrajput87@gmail.com>
2024-12-07 12:33:19 +0530

    portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import {  StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-//import App from './App.jsx'
-//import Header from './components/header/Header.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import Root from './components/layout/root.jsx'
-//import Home from './components/Home/Home.jsx'
+import Root from './components/layout/Root.jsx'
 import About from './components/about/About.jsx'
 import Gallery from './components/Gallery/Gallery.jsx'
 import Contact from './components/contact/Contact.jsx'
