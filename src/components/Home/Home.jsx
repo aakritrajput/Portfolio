a6082dcc2d149c26b60820ffa41a59d5078e16a0
aakritrajput <aakritrajput87@gmail.com>
2025-02-18 22:17:49 +0530

    update resume file reference and add new resume document

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import myResume from './Aakrit_Resume (4).pdf'
+import myResume from './Resume_Aakrit.pdf'
 import { motion } from 'motion/react'
 import myImage from "../../../public/aakritpassphoto.png"
 import OvalComponent from "./OvalComponent.jsx"
